e8dc9768411be7d5cf8553df354a57c7e5901e4b
Huiyicc <1925473403@qq.com>
2024-11-10 18:48:52 +0800

    设置默认位置

--- src/G2pglobal.cpp
+++ src/G2pglobal.cpp
@@ -3,48 +3,47 @@
 #include <memory>
 #include <unordered_set>
 
-namespace Pinyin
-{
-    class G2pGlobal {
-    public:
-        std::filesystem::path path;
-    };
+namespace Pinyin {
+class G2pGlobal {
+public:
+  std::filesystem::path path = std::filesystem::current_path() / "res" / "dict";
+};
 
-    auto m_global = std::make_unique<G2pGlobal>();
+auto m_global = std::make_unique<G2pGlobal>();
 
-    std::filesystem::path dictionaryPath() {
-        return m_global->path;
-    }
+std::filesystem::path dictionaryPath() {
+  return m_global->path;
+}
 
-    void setDictionaryPath(const std::filesystem::path &dir) {
-        m_global->path = dir;
-    }
+void setDictionaryPath(const std::filesystem::path &dir) {
+  m_global->path = dir;
+}
 
-    bool isLetter(char32_t c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-    }
+bool isLetter(char32_t c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
 
-    bool isHanzi(char32_t c) {
-        return c >= 0x4e00 && c <= 0x9fa5;
-    }
+bool isHanzi(char32_t c) {
+  return c >= 0x4e00 && c <= 0x9fa5;
+}
 
-    bool isKana(char32_t c) {
-        return (c >= 0x3040 && c <= 0x309F) || (c >= 0x30A0 && c <= 0x30FF);
-    }
+bool isKana(char32_t c) {
+  return (c >= 0x3040 && c <= 0x309F) || (c >= 0x30A0 && c <= 0x30FF);
+}
 
-    bool isDigit(char32_t c) {
-        return c >= '0' && c <= '9';
-    }
+bool isDigit(char32_t c) {
+  return c >= '0' && c <= '9';
+}
 
-    bool isSpace(char32_t c) {
-        return c == ' ';
-    }
+bool isSpace(char32_t c) {
+  return c == ' ';
+}
 
-    bool isSpecialKana(char32_t c) {
-        static const std::unordered_set<char32_t> specialKana = {
-            U'ャ', U'ュ', U'ョ', U'ゃ', U'ゅ', U'ょ',
-            U'ァ', U'ィ', U'ゥ', U'ェ', U'ォ', U'ぁ', U'ぃ', U'ぅ', U'ぇ', U'ぉ'
-        };
-        return specialKana.find(c) != specialKana.end();
-    }
+bool isSpecialKana(char32_t c) {
+  static const std::unordered_set<char32_t> specialKana = {
+    U'ャ', U'ュ', U'ョ', U'ゃ', U'ゅ', U'ょ',
+    U'ァ', U'ィ', U'ゥ', U'ェ', U'ォ', U'ぁ', U'ぃ', U'ぅ', U'ぇ', U'ぉ'
+  };
+  return specialKana.find(c) != specialKana.end();
+}
 }
